de5c0124aeded236d30a26d7f67aba99a61e316f
zsh2008 <zsh2008@yeah.net>
2018-09-27 21:12:55 +0800

    kNN.py update for KNN test

--- data-analysis/KNN/kNN.py
+++ data-analysis/KNN/kNN.py
@@ -8,7 +8,7 @@ def createDataSet():
     return group, labels
 
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0] #get the colume of the dataSet.
+    dataSetSize = dataSet.shape[0] # .
     # print dataSetSize ; out:4; shape/tile/sum
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
@@ -23,12 +23,18 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1 # set dictory value, if not existed value is 0
-    print classCount
+    print (classCount)
     ###operator模块提供的itemgetter函数用于获取对象的哪些维的数据，参数为一些序号
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    print sortedClassCount[0][0]
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    print (sortedClassCount[0][0])
     return sortedClassCount[0][0]
-
+###################################################################
+### function: Transfer txt file to Matrix                       ###
+### input : File directory and name                             ###
+### output:                                                     ###
+###     -->returnMat, the Matrix data.                          ###
+###     -->classLabelVector, the tag which you want to classify ###
+###################################################################
 def file2matrix(filename):
     #read file's lines
     fr = open(filename)
@@ -42,25 +48,30 @@ def file2matrix(filename):
     #Parse line to a list, to be a matrix of (x, 3)
     index = 0
     for line in fr.readlines():
-        line = line.strip()
-        listFormatLine = line.split('\t')
-        returnMat[index, :] = listFormatLine[0:3]
-        classLabelVector.append(int(listFormatLine[-1]))
+        line = line.strip() # 删掉字符串前面和后面的空格
+        listFormatLine = line.split('\t') # 以\t 为分隔符分割字符串，返回list
+        returnMat[index, :] = listFormatLine[0:3] # 字符串切片赋值给矩阵
+        classLabelVector.append(int(listFormatLine[-1])) # 导数第一个list值强制转换为int， append 至vector。
         index += 1
     return returnMat, classLabelVector
 
-############################################
-### Data-normalizing code                ###
-### newValue = (oldValue-min)/(max-min)  ###
-############################################
+###########################################################
+### function: Data-normalizing code                     ###
+### newValue = (oldValue-min)/(max-min)                 ###
+### input : dataSet, the Matrix data                    ###
+### output:                                             ###
+###     --> normDataSet, the Matrix data after normalize###
+###     --> ranges, the vale of (max-min)               ###
+###     --> minVals, the min value                      ###
+###########################################################
 def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
+    minVals = dataSet.min(0) # 参数0 可以使min 从所有列中选取最小值， axis=0，例子中的矩阵是1x3
+    maxVals = dataSet.max(0) # 所有列中选取最大值， axis=0，
     ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
+    normDataSet = zeros(shape(dataSet)) # 生成与传入矩阵同等维度的全0矩阵, 例子中是 1000x3
+    m = dataSet.shape[0] # 得到矩阵一共有多少行
     #matrix arithmetic
-    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = dataSet - tile(minVals, (m,1)) # tile函数将minVals的单位值叠加至m行，1表示列数不变，之后进行矩阵运算
     normDataSet = normDataSet/tile(ranges, (m,1))
     return normDataSet, ranges, minVals
 
--- data-analysis/KNN/test.py
+++ data-analysis/KNN/test.py
@@ -1,5 +1,4 @@
 from kNN import *
-import matplotlib
 import matplotlib.pyplot as plt
 from numpy import *
 group, labels = createDataSet()
@@ -14,36 +13,59 @@ group, labels = createDataSet()
 ###############################################
 def plt_dating():
     datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
-    print datingDataMat,datingLabels[0:20]
+    print(datingDataMat,datingLabels[0:20])
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     # the last args defined the scatter points' big or color. the corresponding t0, s and c.
     # detail to see:https://matplotlib.org/api/pyplot_api.html
-    ax.scatter(datingDataMat[:,1],datingDataMat[:,0], array(datingLabels), array(datingLabels))
+    ax.scatter(datingDataMat[:,1],datingDataMat[:,0], 20*array(datingLabels), array(datingLabels))
     plt.show()
 
 ###############################################
 ### KNN Classifier testing code for dating site
 ###############################################
 def datingClassTest():
-    hoRatio = 0.6
+    hoRatio = 0.1
     datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
+    # Test suit is hoRatio.
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
 
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],\
-                                     datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d"\
-            %(classifierResult, datingLabels[i])
+                                     datingLabels[numTestVecs:m],15)
+        print ("the classifier came back with: %d, the real answer is: %d"\
+            %(classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1
-    print "the total error rate is: %f" %(errorCount/float(numTestVecs))
+    print ("the total error rate is: %f" %(errorCount/float(numTestVecs)))
+
+def classifyPerson():
+    # Get the info by input(python3)
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input(\
+                "percentage of time spent playing video games?"))
+    ffMiles = float(input(\
+            "frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    inArr = array([ffMiles, percentTats, iceCream])
+
+    # Do KNN classify.
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # Different K value may
+    classifierResult = classify0((inArr-minVals)/ranges, \
+                                 normMat, datingLabels, 60)
+
+    # Print out the result by the tags classified by KNN result(classifierResult).
+    print("You will probably like this person: ", resultList[classifierResult - 1])
+
 
 if __name__=="__main__":
     #plt_dating()
-    datingClassTest()
+    #datingClassTest()
+    classifyPerson()
 
 
